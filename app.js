ffb59912feae9a540300ed9a7f6bcdf35b0b1caf
Eduardo Freitas de Carvalho <eduardofc@dcc.ufrj.br>
2021-07-10 10:17:54 -0300

    Add comments to clarify code

--- app.js
+++ app.js
@@ -9,18 +9,22 @@ const eduardo = JSON.parse(fs.readFileSync('eduardo.json', 'utf-8'));
 http.createServer((request, response) => {
     const params = new URLSearchParams(request.url);
 
-    /* Ignoring favicon requests, maybe I will create a Express.js 
+    /* ignoring favicon requests, maybe I will create a Express.js 
     version that set one server-favicon or find out a way to handle it */
     if (params.has('/favicon.ico')) {
         return;
     }
 
+    /* if flag &beautify=true is provided, start beautify to allow
+    tabulation on JSON.stringfy() */
     const beautify = params.get('beautify') == 'true' ? '\t' : null;
 
     if (params.has('/api')) {
         const data = {};
 
         if (params.has('query')) {
+            /* iterate through all passed queries searching for invalid keys,
+            if a key is invalid, send user a error message in the form of a JSON*/
             for (key of params.get('query').split(',')) {
                 if (eduardo.hasOwnProperty(key)) {
                     data[key] = eduardo[key];
