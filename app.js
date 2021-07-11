3c890e5ae63ccd51c6bfc47fd8b9e101a15c7be2
Eduardo Freitas de Carvalho <eduardofc@dcc.ufrj.br>
2021-07-11 15:56:25 -0300

    Change code structure to use Error Handling instead of the previous method

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ const port = process.env.PORT || 8888;
 
 const eduardo = JSON.parse(fs.readFileSync('eduardo.json', 'utf-8'));
 
-const writeErrorToOutput = (output, code, errorType, errorDescription) => {
-    output.success = false;
-    output.error = {
-        code: code,
-        type: errorType,
-        info: errorDescription
-    };
+class BadRequest extends Error {
+    constructor(code, type, message) {
+        super(message);
+        this.code = code;
+        this.type = type;
+    }
 
-    delete output.data;
-};
+    toObject() {
+        return {
+            success: false,
+            error: {
+                code: this.code,
+                type: this.type,
+                info: this.message
+            }
+        };
+    }
+}
 
 http.createServer((request, response) => {
     const params = new URLSearchParams(request.url);
@@ -28,53 +36,70 @@ http.createServer((request, response) => {
     tabulation on JSON.stringify() */
     const beautify = params.get('beautify') == 'true' ? '\t' : null;
 
-    /* output JSON */
-    const output = {
-        success: true,
-        data: {}
-    };
+    try {
+        /* default output JSON */
+        const output = {
+            success: true,
+            data: {}
+        };
 
-    if (params.has('/api')) {
-        if (params.has('query')) {
-            const query = params.get('query');
+        if (params.has('/api')) {
+            if (params.has('query')) {
+                const query = params.get('query');
 
-            /* If query is empty or all, return default value (all data output) */
-            if (query == '' || query == 'all') {
-                output.data = eduardo;
-            } else {
-                /* iterate through all passed queries searching for invalid keys,
-                if a key is invalid, send user a error message in the form of a JSON*/
-                for (key of query.split(',')) {
-                    if (eduardo.hasOwnProperty(key)) {
-                        output.data[key] = eduardo[key];
-                    } else {
-                        writeErrorToOutput(
-                            output,
-                            (code = 202),
-                            (type = 'invalid_query'),
-                            (info = `Invalid query provided. Valid queries: ${Object.keys(
-                                eduardo
-                            ).join()}`)
-                        );
-
-                        break;
+                /* If query is empty or all, return default value (all data output) */
+                if (query == '' || query == 'all') {
+                    output.data = eduardo;
+                } else {
+                    /* iterate through all passed queries searching for invalid keys,
+                    if a key is invalid, send user a error message in the form of a JSON*/
+                    for (key of query.split(',')) {
+                        if (eduardo.hasOwnProperty(key)) {
+                            output.data[key] = eduardo[key];
+                        } else {
+                            throw new BadRequest(
+                                (code = 202),
+                                (type = 'invalid_query'),
+                                (info = `Invalid query provided. Valid queries: ${Object.keys(
+                                    eduardo
+                                ).join()}`)
+                            );
+                        }
                     }
                 }
+            } else {
+                output.data = eduardo;
             }
         } else {
-            output.data = eduardo;
+            throw new BadRequest(
+                (code = 202),
+                (type = 'invalid_endpoint'),
+                (info = 'Invalid endpoint used. Consult documentation for further information.')
+            );
         }
-    } else {
-        writeErrorToOutput(
-            output,
-            (code = 202),
-            (type = 'invalid_endpoint'),
-            (info = 'Invalid endpoint used. Consult documentation for further information.')
-        );
-    }
 
-    response.write(JSON.stringify(output, null, beautify));
-    response.end();
+        response.write(JSON.stringify(output, null, beautify));
+    } catch (e) {
+        if (e instanceof BadRequest) {
+            /* If error is a BadRequest, return it's object to user */
+            response.write(JSON.stringify(e.toObject(), null, beautify));
+        } else {
+            /* If a unexpected error is thrown, return code 500 (Internal Error) to user */
+            response.write(
+                JSON.stringify(
+                    new BadRequest(
+                        (code = 500),
+                        (type = 'unexpected_error'),
+                        (info = 'A unexpected error occurred, try again.')
+                    ).toObject(),
+                    null,
+                    beautify
+                )
+            );
+        }
+    } finally {
+        response.end();
+    }
 }).listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
